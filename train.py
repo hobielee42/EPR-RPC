43e0cd233b91c3a4892feb2f2cb12c0a157b26a6
Hobie Li <hobbylee6@gmail.com>
2023-08-23 11:51:37 +0800

    train init

--- train.py
+++ train.py
@@ -4,29 +4,29 @@ data_config = {
     "snli": {
         "train": {
             "tokens": "data/encodings/snli/tokens/train_tokens.pkl",
-            "alignments": "data/encodings/snli/alignment/train_alignments.pkl",
+            "alignments": "data/encodings/snli/alignments/train_alignments.pkl",
         },
         "val": {
             "tokens": "data/encodings/snli/tokens/val_tokens.pkl",
-            "alignments": "data/encodings/snli/alignment/val_alignments.pkl",
+            "alignments": "data/encodings/snli/alignments/val_alignments.pkl",
         },
         "test": {
             "tokens": "data/encodings/snli/tokens/test_tokens.pkl",
-            "alignments": "data/encodings/snli/alignment/test_alignments.pkl",
+            "alignments": "data/encodings/snli/alignments/test_alignments.pkl",
         },
     },
     "mnli": {
         "train": {
             "tokens": "data/encodings/mnli/tokens/train_tokens.pkl",
-            "alignments": "data/encodings/mnli/alignment/train_alignments.pkl",
+            "alignments": "data/encodings/mnli/alignments/train_alignments.pkl",
         },
         "val": {
             "tokens": "data/encodings/mnli/tokens/val_tokens.pkl",
-            "alignments": "data/encodings/mnli/alignment/val_alignments.pkl",
+            "alignments": "data/encodings/mnli/alignments/val_alignments.pkl",
         },
         "test": {
             "tokens": "data/encodings/mnli/tokens/test_tokens.pkl",
-            "alignments": "data/encodings/mnli/alignment/test_alignments.pkl",
+            "alignments": "data/encodings/mnli/alignments/test_alignments.pkl",
         },
     },
 }
